ddaa187f3a899ec09d023ccf4552abc8e6be8133
montanari2019 <ikaro.montanari31@gmail.com>
2022-06-12 15:42:08 -0400

    usando props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-function App() {
+import { Post } from "./Post";
+
+export function App() {
   return (
     <div>
-      <h1>Olá mundo</h1>
+      <h1>Bem vindo aos posts</h1>
+
+      <br />
+
+      <Post
+        author="Ikaro Montanari"
+        comment="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam provident aspernatur excepturi tenetur veniam, explicabo tempore sapiente quis vel, incidunt officiis enim porro iure minima, neque vitae. Repudiandae, laudantium cumque."
+      />
+
+      <Post
+        author="Adam Bortolozo"
+        comment="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam provident aspernatur excepturi tenetur veniam, explicabo tempore sapiente quis vel, incidunt officiis enim porro iure minima, neque vitae. Repudiandae, laudantium cumque."
+      />
     </div>
   );
 }
-
-export default App;
